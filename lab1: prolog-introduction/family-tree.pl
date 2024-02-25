aeeaf97b88dda7692ac83222e3583488262805ba
programat <egordets@gmail.com>
2024-02-25 17:27:14 +0300

    feat: task2 of lab1 is complete docs: update for some comments in code fix: family database is changed: a second parent was added so there were husbands and wives

--- lab1: prolog-introduction/family-tree.pl
+++ lab1: prolog-introduction/family-tree.pl
@@ -1,4 +1,4 @@
-% consult("/Users/egorken/IdeaProjects/test/src/lab1.pl").
+% family-tree.pl
 
 % Факты о членах семьи
 man(john).
@@ -7,20 +7,35 @@ man(jack).
 man(joe).
 man(bob).
 man(jim).
+man(tom).
 
 woman(mary).
 woman(susan).
 woman(lisa).
 woman(emma).
+woman(anne).
+woman(aurora).
+woman(isabella).
 
 % Отношения "родитель"
 parent(john, mary).
+parent(isabella, mary).
+
 parent(mary, susan).
 parent(mary, lisa).
+parent(tom, susan).
+parent(tom, lisa).
+
 parent(mike, joe).
 parent(mike, jack).
+parent(anne, joe).
+parent(anne, jack).
+
 parent(joe, bob).
 parent(joe, jim).
+parent(aurora, bob).
+parent(aurora, jim).
+
 parent(lisa, emma).
 
 
@@ -48,12 +63,20 @@ brother(X, Y) :- man(X), parent(P, X), parent(P, Y), X \= Y.
 % Предикат brother(X) для вывода всех братьев X
 brother(X) :- man(Y), brother(Y, X), write(Y), nl.
 
-% Предикат b_s(X,Y)
+% Предикат b_s(X,Y): X и Y - родные брат и сестра?
 b_s(X, Y) :- parent(P, X), parent(P, Y), X \= Y.
 
-% Предикат b_s(X)
+% Предикат b_s(X) для вывода всех братьев или сестер X
 b_s(X) :- b_s(X, Y), write(Y), nl.
 
 
+% 1.2.6
+% Предикат daughter(X, Y):  X - дочь Y?
+daughter(X, Y) :- woman(X), parent(Y, X).
+% Предикат brother(X) для вывода дочери X
+daughter(X) :- woman(Y), parent(X, Y), write(Y), fail.
 
-
+% Предикат husband(X, Y):  X - муж Y?
+husband(X, Y) :- man(X), parent(X, C), parent(Y, C), X \= Y.
+% Предикат brother(X) для вывода мужа X
+husband(X) :- man(Y), parent(X, C), parent(Y, C), X \= Y, write(Y).

311be75ec0df1fd65b19bc44baf099f64776544f
programat <egordets@gmail.com>
2024-02-25 19:47:04 +0300

    feat: task3 of lab1 is complete fix: update family database to address visibility issue with aunts and grandmothers docs: update code comments and add family tree scheme

--- lab1: prolog-introduction/family-tree.pl
+++ lab1: prolog-introduction/family-tree.pl
@@ -8,6 +8,7 @@ man(joe).
 man(bob).
 man(jim).
 man(tom).
+man(peter).
 
 woman(mary).
 woman(susan).
@@ -16,10 +17,16 @@ woman(emma).
 woman(anne).
 woman(aurora).
 woman(isabella).
+woman(kate).
 
 % Отношения "родитель"
 parent(john, mary).
+parent(john, anne).
 parent(isabella, mary).
+parent(isabella, anne).
+
+parent(john, kate).
+parent(isabella, kate).
 
 parent(mary, susan).
 parent(mary, lisa).
@@ -37,6 +44,17 @@ parent(aurora, bob).
 parent(aurora, jim).
 
 parent(lisa, emma).
+parent(peter, emma).
+
+
+%                   John & Isabella
+%                      /  |  \
+%                     /   |   \
+%           Tom & Mary  Kate   Anne & Mike
+%              / \                 | \
+%         Susan   Lisa & Peter  Jack  Joe & Aurora
+%                     /                 /  \
+%                   Emma              Bob   Jim
 
 
 % Педикат men
@@ -63,7 +81,7 @@ brother(X, Y) :- man(X), parent(P, X), parent(P, Y), X \= Y.
 % Предикат brother(X) для вывода всех братьев X
 brother(X) :- man(Y), brother(Y, X), write(Y), nl.
 
-% Предикат b_s(X,Y): X и Y - родные брат и сестра?
+% Предикат b_s(X,Y): X и Y - родные брат и сестра (сестра и брат / сестра и сестра / брат и брат)?
 b_s(X, Y) :- parent(P, X), parent(P, Y), X \= Y.
 
 % Предикат b_s(X) для вывода всех братьев или сестер X
@@ -80,3 +98,28 @@ daughter(X) :- woman(Y), parent(X, Y), write(Y), fail.
 husband(X, Y) :- man(X), parent(X, C), parent(Y, C), X \= Y.
 % Предикат brother(X) для вывода мужа X
 husband(X) :- man(Y), parent(X, C), parent(Y, C), X \= Y, write(Y).
+
+% 1.3.6
+% Предикат grand_ma(X, Y): X - бабушка Y?
+grand_ma(X, Y) :- parent(X, P), parent(P, Y), woman(X).
+% Предикат grand_ma1(X, Y): X - бабушка Y? с использованием готовых предиктатов
+grand_ma1(X, Y) :- mother(X, P), parent(P, Y).
+
+% Предикат grand_mas(X) для вывода всех бабушек X
+grand_mas(X) :- grand_ma(Y, X), write(Y), nl, fail.
+
+% Предикат grand_ma_and_da(X, Y): X - бабушка и Y - внучка (или наоборот)?
+grand_ma_and_da(X, Y) :- woman(Y), woman(X), (grand_ma(X, Y); grand_ma(Y, X)).
+
+
+% Предикат sister(X, Y): X - сестра Y?
+sister(X, Y) :- woman(X), b_s(X, Y).
+% Предикат aunt(X, Y): X - тетя Y?
+aunt(X, Y) :- woman(X), parent(P, Y), sister(X, P).
+
+% Предикат aunts1(X): выводит всех тетей X с повторениями :(
+aunts1(X) :- aunt(Y, X), write(Y), nl, fail.
+
+% Предикат aunts(X): выводит всех тетей X
+aunts(X) :- findall(Y, aunt(Y, X), Aunts), list_to_set(Aunts, UniqueAunts), member(Y, UniqueAunts), write(Y), nl, fail.
+

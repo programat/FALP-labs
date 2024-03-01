370a7d7e9e87229076a722035042c3b8123ba3b4
programat <egordets@gmail.com>
2024-03-02 02:47:42 +0300

    docs: update code comments according to prolog notation

--- lab1: prolog-introduction/family-tree.pl
+++ lab1: prolog-introduction/family-tree.pl
@@ -51,69 +51,94 @@ parent(duhovlad,zlatomir).
 parent(zhdana,zdislava).
 parent(zhdana,zlatomir).
 
-% Педикат men
+% Predicate men
+% Lists all men in the database.
 men :- man(X), write(X), nl, fail.
 
-% Педикат women
+% Predicate women
+% Lists all women in the database.
 women :- woman(X), write(X), nl, fail.
 
-% Предикат "является родителем" X - родитель Y
-is_parent(X, Y) :- parent(X, Y), write(X), write(' is a parent of '), write(Y), nl, fail.
+% Predicate is_parent(+Parent:atom, +Child:atom)
+% True if Parent is a parent of Child.
+is_parent(Parent, Child) :- parent(Parent, Child), write(Parent), write(' is a parent of '), write(Child), nl, fail.
 
-% Предикат children(X) для вывода всех детей X
-children(X) :- parent(X, Y), write(Y), nl, fail.
+% Predicate children(+Parent:atom)
+% Lists all children of Parent.
+children(Parent) :- parent(Parent, Child), write(Child), nl, fail.
 
-% Предикат mother(X, Y):   X - мать Y?
-mother(X, Y) :- woman(X), parent(X, Y).
+% Predicate mother(+Mother:atom, +Child:atom)
+% True if Mother is the mother of Child.
+mother(Mother, Child) :- woman(Mother), parent(Mother, Child).
 
-% Предикат mother(X)  выводит маму X
-mother(X) :- woman(Y), parent(Y, X), write(Y), fail.
+% Predicate mother(+Child:atom)
+% Lists all mothers of Child.
+mother(Child) :- woman(Mother), parent(Mother, Child), write(Mother), fail.
 
-% Предикат brother(X, Y):   X - брат Y?
-brother(X, Y) :- man(X), parent(P, X), parent(P, Y), X \= Y.
+% Predicate brother(+Brother:atom, +Sibling:atom)
+% True if Brother is a brother of Sibling.
+brother(Brother, Sibling) :- man(Brother), parent(Parent, Brother), parent(Parent, Sibling), Brother \= Sibling.
 
-% Предикат brother(X) для вывода всех братьев X
-brother(X) :- man(Y), brother(Y, X), write(Y), nl.
+% Predicate brother(+Sibling:atom)
+% Lists all brothers of Sibling.
+brother(Sibling) :- man(Brother), brother(Brother, Sibling), write(Brother), nl.
 
-% Предикат b_s(X,Y): X и Y - родные брат и сестра (сестра и брат / сестра и сестра / брат и брат)?
-b_s(X, Y) :- parent(P, X), parent(P, Y), X \= Y.
+% Predicate b_s(+Sibling1:atom, +Sibling2:atom)
+% True if Sibling1 and Sibling2 are full siblings.
+b_s(Sibling1, Sibling2) :- parent(Parent, Sibling1), parent(Parent, Sibling2), Sibling1 \= Sibling2.
 
-% Предикат b_s(X) для вывода всех братьев или сестер X
-b_s(X) :- b_s(X, Y), write(Y), nl.
+% Predicate b_s(+Sibling:atom)
+% Lists all full siblings of Sibling.
+b_s(Sibling) :- b_s(Sibling, Sibling2), write(Sibling2), nl.
 
+% Task 2.6 ---
 
-% 2.6
-% Предикат daughter(X, Y):  X - дочь Y?
-daughter(X, Y) :- woman(X), parent(Y, X).
-% Предикат brother(X) для вывода дочери X
-daughter(X) :- woman(Y), parent(X, Y), write(Y), fail.
+% Predicate daughter(+Daughter:atom, +Parent:atom)
+% True if Daughter is a daughter of Parent.
+daughter(Daughter, Parent) :- woman(Daughter), parent(Parent, Daughter).
 
-% Предикат husband(X, Y):  X - муж Y?
-husband(X, Y) :- man(X), parent(X, C), parent(Y, C), X \= Y.
-% Предикат brother(X) для вывода мужа X
-husband(X) :- man(Y), parent(X, C), parent(Y, C), X \= Y, write(Y).
+% Predicate daughter(+Parent:atom)
+% Lists all daughters of Parent.
+daughter(Parent) :- woman(Daughter), parent(Parent, Daughter), write(Daughter), nl, fail.
 
-% 3.6
-% Предикат grand_ma(X, Y): X - бабушка Y?
-grand_ma(X, Y) :- parent(X, P), parent(P, Y), woman(X).
-% Предикат grand_ma1(X, Y): X - бабушка Y? с использованием готовых предиктатов
-grand_ma1(X, Y) :- mother(X, P), parent(P, Y).
+% Predicate husband(+Husband:atom, +Wife:atom)
+% True if Husband is the husband of Wife.
+husband(Husband, Wife) :- man(Husband), parent(Husband, Child), parent(Wife, Child), Husband \= Wife.
 
-% Предикат grand_mas(X) для вывода всех бабушек X
-grand_mas(X) :- grand_ma(Y, X), write(Y), nl, fail.
+% Predicate husband(+Wife:atom)
+% Lists all husbands of Wife.
+husband(Wife) :- man(Husband), parent(Husband, Child), parent(Wife, Child), Husband \= Wife, write(Husband).
 
-% Предикат grand_ma_and_da(X, Y): X - бабушка и Y - внучка (или наоборот)?
-grand_ma_and_da(X, Y) :- woman(Y), woman(X), (grand_ma(X, Y); grand_ma(Y, X)).
+% Task 3.6 ---
 
+% Predicate grand_ma(+Grandma:atom, +Grandchild:atom)
+% True if Grandma is the grandmother of Grandchild.
+grand_ma(Grandma, Grandchild) :- parent(Grandma, Parent), parent(Parent, Grandchild), woman(Grandma).
 
-% Предикат sister(X, Y): X - сестра Y?
-sister(X, Y) :- woman(X), b_s(X, Y).
-% Предикат aunt(X, Y): X - тетя Y?
-aunt(X, Y) :- woman(X), parent(P, Y), sister(X, P).
+% Predicate grand_ma1(+Grandma:atom, +Grandchild:atom)
+% True if Grandma is the grandmother of Grandchild using predefined predicates.
+grand_ma1(Grandma, Grandchild) :- mother(Grandma, Parent), parent(Parent, Grandchild).
 
-% Предикат aunts1(X): выводит всех тетей X с повторениями :(
-aunts1(X) :- aunt(Y, X), write(Y), nl, fail.
+% Predicate grand_mas(+Grandchild:atom)
+% Lists all grandmothers of Grandchild.
+grand_mas(Grandchild) :- grand_ma(Grandma, Grandchild), write(Grandma), nl, fail.
 
-% Предикат aunts(X): выводит всех тетей X
-aunts(X) :- findall(Y, aunt(Y, X), Aunts), list_to_set(Aunts, UniqueAunts), member(Y, UniqueAunts), write(Y), nl, fail.
+% Predicate grand_ma_and_da(+Grandparent:atom, +Grandchild:atom)
+% True if Grandparent is the grandmother or grandfather of Grandchild.
+grand_ma_and_da(Grandparent, Grandchild) :- woman(Grandchild), (grand_ma(Grandparent, Grandchild); grand_ma(Grandchild, Grandparent)).
 
+% Predicate sister(+Sister:atom, +Sibling:atom)
+% True if Sister is a sister of Sibling.
+sister(Sister, Sibling) :- woman(Sister), b_s(Sister, Sibling).
+
+% Predicate aunt(+Aunt:atom, +NieceNephew:atom)
+% True if Aunt is the aunt of NieceNephew.
+aunt(Aunt, NieceNephew) :- woman(Aunt), parent(Parent, NieceNephew), sister(Aunt, Parent).
+
+% Predicate aunts1(+NieceNephew:atom)
+% Lists all aunts of NieceNephew (with repetitions).
+aunts1(NieceNephew) :- aunt(Aunt, NieceNephew), write(Aunt), nl, fail.
+
+% Predicate aunts(+NieceNephew:atom)
+% Lists all unique aunts of NieceNephew.
+aunts(NieceNephew) :- findall(Aunt, aunt(Aunt, NieceNephew), Aunts), list_to_set(Aunts, UniqueAunts), member(Aunt, UniqueAunts), write(Aunt), nl, fail.

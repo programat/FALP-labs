bf30ca7464d4f65f0b73bdc262d00965abbaad52
programat <egordets@gmail.com>
2024-05-27 07:55:24 +0300

    ref: rewrite task3 of lab6 in more like functional style style: minor changes

--- lab6: kotlin-collections/lab6-kotlin/src/CollectionTasks.kt
+++ lab6: kotlin-collections/lab6-kotlin/src/CollectionTasks.kt
@@ -1,17 +1,14 @@
 class CollectionTasks {
-    // CollectionTasks.kt
+    // task 3 ---
 
     // 1.6. Cyclic shift of array elements to the left by three positions
-    fun shiftLeft(array: List<Int>, positions: Int): List<Int> {
-        val shiftedPositions = positions % array.size
-        return array.drop(shiftedPositions) + array.take(shiftedPositions)
-    }
+    fun shiftLeft(array: List<Int>, positions: Int): List<Int> =
+        array.drop( positions % array.size) + array.take( positions % array.size)
 
     // 1.7. Cyclic shift of array elements to the right by two positions
-    fun shiftRight(array: List<Int>, positions: Int): List<Int> {
-        val shiftedPositions = positions % array.size
-        return array.takeLast(shiftedPositions) + array.dropLast(shiftedPositions)
-    }
+    fun shiftRight(array: List<Int>, positions: Int): List<Int> =
+        array.takeLast(positions % array.size) + array.dropLast(positions % array.size)
+
 
     // 1.18. Finding elements located before the first minimum
     fun elementsBeforeFirstMin(array: List<Int>): List<Int> {
@@ -20,19 +17,18 @@ class CollectionTasks {
     }
 
     // 1.24. Finding the two largest elements
-    fun twoLargestElements(array: List<Int>): List<Int> {
-        return array.sortedDescending().take(2)
-    }
+    fun twoLargestElements(array: List<Int>): List<Int> =
+        array.sortedDescending().take(2)
 
     // 1.27. Cyclic shift of array elements to the left by one position
-    fun shiftLeftByOne(array: List<Int>): List<Int> {
-        return array.drop(1) + array.take(1)
-    }
+    fun shiftLeftByOne(array: List<Int>): List<Int> =
+        array.drop(1) + array.take(1)
+
 
     // 1.36. Finding the maximum odd element
-    fun maxOddElement(array: List<Int>): Int? {
-        return array.filter { it % 2 != 0 }.maxOrNull()
-    }
+    fun maxOddElement(array: List<Int>): Int? =
+        array.filter { it % 2 != 0 }.maxOrNull()
+
 
     fun main() {
         val array = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
